5f3f4fc42eeb2e415bb56187ef42fa2228d1a8db
Kaidan <fishstacks@protonmail.com>
2018-07-04 17:55:09 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Discmulticast - multicasting discord messages
+This bot was created to allow one-to-many messages on discord, meaning you can send a single message to one channel and have it automatically sent to many others. It is currently early in development and more features are planned, but the good news is it's usable right now.
+
+It should work on both windows and linux, however windows support is untested.
+
+## Requirements
+The bot requires the latest version of python to be installed and uses the rewrite branch of discord.py, plus some other libraries. Everything required can be easily installed using `pip` via `pip install -r requirements.txt`
+
+## Running and configuration
+The bot largely operates based on a TOML configuration file, for an example see the [example config](example_config.txt).
+
+The config file needs to be specified in the `DISCMULTI_CONFIG` environment variable in order for it to be loaded by the bot.
+this means you need to run the bot like this:
+```
+DISCMULTI_CONFIG=config.toml python discmulticast.py
+```
+The above should work for both windows and linux.
+
+## Feature list
+- [ ] Multicast message edits and deletes
+- [ ] Allow for multiple source addresses
+- [ ] Send messages from multiple users, not just bot user
+
+## License
+This program is released under the terms of the ISC license, details can be found in the [license file](LICENSE.txt).
---
+++ discmulticast.py
@@ -0,0 +1,74 @@
+#
+# discmulticast.py
+#
+# discord multicast - Multicasting messages on Discord
+# Copyright (c) 2018 Kaidan (fishstacks@protonmail)
+#
+# discord multicast is available free of charge under the terms of the ISC
+# license. You are free to redistribute and/or modify it under those
+# terms. It is distributed in the hopes that it will be useful, but
+# WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+
+import logging
+from os import environ
+import discord
+import chromalog
+import toml
+
+# Setup for logging
+# Log output format
+LOG_FORMAT = '[%(levelname)s] %(asctime)s %(name)s: %(message)s'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+# Use chromalog for coloured output
+chromalog.basicConfig(
+    level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+logger = logging.getLogger('discmulticast')
+# Shut the discord module up
+logging.getLogger('discord').setLevel(logging.ERROR)
+logging.getLogger('websockets').setLevel(logging.ERROR)
+
+# Load config
+logger.info('Loading config')
+config = toml.load(environ['DISCMULTI_CONFIG'])
+channels = config['dest_ids']
+# Create bot instance
+bot = discord.Client()
+
+
+@bot.event
+async def on_ready():
+    """Event fired when bot is connected to discord websocket"""
+
+    logger.info(
+        f'Starting bot as user {bot.user.name}#{bot.user.discriminator}')
+    logger.info(
+        f'Listening for messages in \"{bot.get_channel(config["src_id"]).name}({config["src_id"]})\"'
+        f'and multicasting them to {len(config["dest_ids"])} other channels')
+
+
+@bot.event
+async def on_message(msg):
+    """Event fired every time the bot sees a new message"""
+
+    if msg.channel.id != config['src_id'] or msg.author.id != bot.user.id:
+        return
+
+    logger.info(f'Got message with ID {msg.id}, multicasting')
+    # TODO: make edits/deletes multicasted
+    for channel_id in channels:
+        channel = bot.get_channel(channel_id)
+        async with channel.typing():
+            await channel.send(msg.content)
+
+
+# Start discord client
+logger.info('Creating client and connecting to Discord API')
+
+# Check if we need to run as a selfbot
+if config['is_selfbot']:
+    logger.warning('Running as selfbot - THIS CAN GET YOU BANNED')
+    bot.run(config['discord_token'], bot=False)
+else:
+    logger.info('Running as regular bot')
+    bot.run(config['discord_token'])
