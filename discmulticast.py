3e2fd089edf7c7a1fa550881c38e81c424f24590
Kaidan <fishstacks@protonmail.com>
2018-07-09 00:06:01 +1000

    Added ability to multicast message deletes

--- discmulticast.py
+++ discmulticast.py
@@ -10,7 +10,8 @@
 # WITHOUT ANY WARRANTY. See the LICENSE file for more details.
 
 import logging
-from os import environ
+import os
+from collections import defaultdict
 import discord
 import chromalog
 import toml
@@ -30,23 +31,33 @@ logging.getLogger('websockets').setLevel(logging.ERROR)
 
 # Load config
 logger.info('Loading config')
-config = toml.load(environ['DISCMULTI_CONFIG'])
+config = toml.load(os.environ['DISCMULTI_CONFIG'])
 destination_channels = config['dest_ids']
+
 
 # Create bot class
 class Bot(discord.Client):
     """Class implementing bot functionality"""
 
+    def __init__(self):
+        # Stores the ID of a message from a source channel as key and a list of delete
+        # coroutines corresponding to messages in destination channels as values
+        # Used to multicast message deletes
+        self.message_mapping = defaultdict(list)
+        super().__init__()
+
     def check(event_handler):
         """
         Used as a decorator to check if the bot should act on received events
         """
+
         async def do_checks(self, msg, *args):
             if msg.channel.id != config['src_id'] or msg.author != self.user:
                 # Ignore input we don't care about
                 return
             else:
                 await event_handler(self, msg)
+
         return do_checks
 
     async def on_ready(self):
@@ -58,7 +69,8 @@ class Bot(discord.Client):
             f'Starting bot as user {self.user.name}#{self.user.discriminator}')
         logger.info(
             f'Listening for messages in \"#{self.get_channel(config["src_id"]).name}\" ({config["src_id"]})'
-            f' and multicasting them to {len(config["dest_ids"])} other channels')
+            f' and multicasting them to {len(config["dest_ids"])} other channels'
+        )
 
     @check
     async def on_message(self, msg):
@@ -70,7 +82,27 @@ class Bot(discord.Client):
         for channel_id in destination_channels:
             channel = self.get_channel(channel_id)
             async with channel.typing():
-                await channel.send(msg.content)
+                dest_message = await channel.send(msg.content)
+                # Store delete coroutines directly
+                # because we can't do msg ID lookups without bot account
+                self.message_mapping[msg.id].append(dest_message.delete)
+
+    @check
+    async def on_message_delete(self, msg):
+        """
+        Event fired when a message is deleted
+
+        May not be triggered if message is not in local cache
+        which happens in large guilds or with old messages
+
+        Used to check for deletes and to broadcast them to destination channels
+        """
+        logger.info(f"Got delete event for message ID {msg.id}, multicasting")
+        for delete_message_func in self.message_mapping[msg.id]:
+            async with msg.channel.typing():
+                await delete_message_func()
+        del self.message_mapping[msg.id]
+
 
 # Start discord client
 logger.info('Creating client and connecting to Discord API')
