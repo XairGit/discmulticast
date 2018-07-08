bb85ad536962f05c1d83666740b5787daadf29ae
Kaidan <fishstacks@protonmail.com>
2018-07-08 17:50:46 +1000

    Use bot class instead of functions and decorators

--- discmulticast.py
+++ discmulticast.py
@@ -32,39 +32,39 @@ logging.getLogger('websockets').setLevel(logging.ERROR)
 logger.info('Loading config')
 config = toml.load(environ['DISCMULTI_CONFIG'])
 channels = config['dest_ids']
-# Create bot instance
-bot = discord.Client()
 
+# Create bot class
+class Bot(discord.Client):
+    """Class implementing bot functionality"""
 
-@bot.event
-async def on_ready():
-    """Event fired when bot is connected to discord websocket"""
+    def __init__(self):
+        super().__init__()
 
-    logger.info(
-        f'Starting bot as user {bot.user.name}#{bot.user.discriminator}')
-    logger.info(
-        f'Listening for messages in \"#{bot.get_channel(config["src_id"]).name}\" ({config["src_id"]})'
-        f' and multicasting them to {len(config["dest_ids"])} other channels')
+    async def on_ready(self):
+        """Event fired when bot is connected to discord websocket"""
 
+        logger.info(
+            f'Starting bot as user {self.user.name}#{self.user.discriminator}')
+        logger.info(
+            f'Listening for messages in \"#{self.get_channel(config["src_id"]).name}\" ({config["src_id"]})'
+            f' and multicasting them to {len(config["dest_ids"])} other channels')
 
-@bot.event
-async def on_message(msg):
-    """Event fired every time the bot sees a new message"""
+    async def on_message(self, msg):
+        """Event fired every time the bot sees a new message"""
 
-    if msg.channel.id != config['src_id'] or msg.author.id != bot.user.id:
-        return
+        if msg.channel.id != config['src_id'] or msg.author != self.user:
+            return
 
-    logger.info(f'Got message with ID {msg.id}, multicasting')
-    # TODO: make edits/deletes multicasted
-    for channel_id in channels:
-        channel = bot.get_channel(channel_id)
-        async with channel.typing():
-            await channel.send(msg.content)
-
+        logger.info(f'Got message with ID {msg.id}, multicasting')
+        # TODO: make edits/deletes multicasted
+        for channel_id in channels:
+            channel = self.get_channel(channel_id)
+            async with channel.typing():
+                await channel.send(msg.content)
 
 # Start discord client
 logger.info('Creating client and connecting to Discord API')
-
+bot = Bot()
 # Check if we need to run as a selfbot
 if config['is_selfbot']:
     logger.warning('Running as selfbot - THIS CAN GET YOU BANNED')
